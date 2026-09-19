13863f13970076e4366498fe58ed643b0decc0b2
Sairash Chaudhary <sairash0323@gmail.com>
2026-09-19 11:55:50 +0545

    exploring validation

--- src/app.js
+++ src/app.js
@@ -1,24 +1,109 @@
 const express = require("express");
-const {User} = require("./models/user");
+const { User } = require("./models/user");
+const validator = require("validator");
 
 const { connectDB } = require("./config/database");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Sabrina",
-    lastName: "Carpenter",
-    email: "sabrina@gmail.com",
-    password: "54321",
-    age: 24,
-    gender: "Female",
-    hairColor: "brown"
-  };
+app.use(express.json()); // middleware, it will convert all the json data that we
+// send from client or postman to js object
 
-  const user = new User(userObj);
-  await user.save();
-  res.send("User data saved.");
+app.post("/signup", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  try {
+    const user = new User({ firstName, lastName, email, password });
+    await user.save();
+    res.send("User data saved.");
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const emailId = req.body.email;
+  try {
+    if (!emailId) {
+      throw new Error("Email is required to get user data.");
+    }
+    const users = await User.find({ email: emailId });
+    if (users.length === 0) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong.");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    if (allUsers.length === 0) {
+      res.status(404).send("No Users found.");
+    } else {
+      res.send(allUsers);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong.");
+  }
+});
+
+app.delete("/user/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    if (!userId) {
+      throw new Error("User id is required.");
+    }
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send("User deleted successfully.");
+    }
+  } catch (err) {
+    res.status(400).send("Invalid Email Id or deletion operation failed.");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const data = req.body;
+  console.log(data);
+  try {
+    const ALLOWED_UPDATES = [
+      "photoUrl",
+      "age",
+      "about",
+      "skills",
+      "password",
+      "gender",
+    ];
+
+    const fieldsToUpdate = Object.keys(data);
+    if (fieldsToUpdate.length === 0) {
+      throw new Error("Request body cannot be empty.");
+    }
+    const isAllowedUpdates =fieldsToUpdate.every((k) => {
+      return ALLOWED_UPDATES.includes(k);
+    });
+    if (!isAllowedUpdates) {
+      throw new Error("can't update the value.");
+    }
+    const updatedUser = await User.findByIdAndUpdate(userId, data, {
+      runValidators: true,
+      returnDocument: "after",
+    });
+    if (!updatedUser) {
+      res.status(404).send("User not found.");
+    } else {
+      res.send("User data updated successfully!");
+    }
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED. " + err.message);
+  }
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,81 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
+      type: String,
+      required: [true, "First name is required."],
+      trim: true,
+      minLength: [3, "First name must be at least 3 characters."],
+      maxLength: [20, "First name cannot exceed 20 characters."],
     },
     lastName: {
-        type:String,
+      type: String,
+      required: [true, "Last name is required."],
+      trim: true,
+      minLength: [3, "Last name must be at least 3 characters."],
+      maxLength: [20, "Last name cannot exceed 20 characters."],
     },
-    email:{
-        type: String,
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email format.");
+        }
+      },
     },
     password: {
-        type: String,
+      type: String,
+      required: [true, "Password is required."],
+      validate(value){
+        if(!validator.isStrongPassword(value)){
+          throw new Error("Enter a strong password.")
+        }
+      }
     },
     age: {
-        type: Number,
+      type: Number,
+      min: [18, "Age must be at least 18."],
+      max: [100, "Age cannot exceed 100."],
     },
-    gender:{
-        type: String,
-    }
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "others"],
+        message: "{VALUE} is not a valid gender.",
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://kristalle.com/wp-content/uploads/2020/07/dummy-profile-pic-1.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid URL format.");
+        }
+      },
+    },
+    about: {
+      type: String,
+      minLength: [5, "About section must be at least 5 characters."],
+      maxLength: [50, "About section cannot exceed 50 characters."],
+    },
+    skills: {
+      type: [String],
+      validate: [
+        (val) => val.length <= 10,
+        "Skills list cannot exceed 10 items.",
+      ],
+    },
+  },
+  { timestamps: true },
+);
 
-})
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model("User", userSchema)
-
-module.exports = {
-    User
-}
+module.exports = { User };
